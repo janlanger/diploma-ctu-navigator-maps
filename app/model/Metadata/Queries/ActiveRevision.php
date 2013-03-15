579a24d1f173cdd3201219f44d17ec0437b8d4be
Jan Langer <langeja1@fit.cvut.cz>
2013-03-15 22:12:27 +0100

    ...

---
+++ app/model/Metadata/Queries/ActiveRevision.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Jan
+ * Date: 13.3.13
+ * Time: 21:47
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Maps\Model\Metadata\Queries;
+
+
+use Maps\Model\Floor\Floor;
+use Maps\Model\Persistence\IQueryable;
+use Maps\Model\Persistence\QueryObjectBase;
+
+class ActiveRevision extends QueryObjectBase {
+
+    private $floor;
+
+    function __construct(Floor $floor)
+    {
+        $this->floor = $floor;
+    }
+
+
+    /**
+     * @param IQueryable $repository
+     * @return \Doctrine\ORM\Query|\Doctrine\ORM\QueryBuilder
+     */
+    protected function doCreateQuery(IQueryable $repository)
+    {
+        return $repository->createQueryBuilder("r")->select("r")
+            ->where("r.floor = :floor")
+            ->andWhere("r.published = true")
+            ->setParameter("floor", $this->floor);
+    }
+}
