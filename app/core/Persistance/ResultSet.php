7d46edcd81a8907a5f5ad4205f19c2e2d9f09f33
Jan Langer <langeja1@fit.cvut.cz>
2013-03-27 22:11:23 +0100

    reverted result set iterator

--- app/core/Persistance/ResultSet.php
+++ app/core/Persistance/ResultSet.php
@@ -157,7 +157,7 @@ class ResultSet extends Nette\Object implements \Countable, \IteratorAggregate
 	public function getIterator()
 	{
 		try {
-            return $this->getPaginatedQuery()->getIterator();
+			return new \ArrayIterator($this->query->execute());
 
 		} catch (ORMException $e) {
 			throw new \Maps\QueryException($e, $this->query, $e->getMessage());
